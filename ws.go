984a06e642d3865e53a755db1b2354d8adc0aa0b
wishforgood <616251828@qq.com>
2022-01-21 01:13:56 +0800

    Update ws.go

--- ws.go
+++ ws.go
@@ -93,14 +93,14 @@ type BiliConfJson struct {
 //connect websocket
 //go receiving(){if live, chan<-download}
 //go heartbeat()
-var urlId int = 23260932
+var urlId int = 21627536
 
 //var urlId int = 220416
 
 //var urlId int = 220416
 var downloadDir string = "r"
 var downloadChan chan time.Time
-var roomId int = 23260932
+var roomId int = 21627536
 
 func main() {
 	//var urlId int = 220416
